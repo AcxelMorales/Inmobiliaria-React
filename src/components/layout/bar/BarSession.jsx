4d7a243e2151016020ce126d82f1c504924c567a
AcxelMorales <acxelmorales97@gmail.com>
2020-02-05 12:25:53 -0600

    Routing v2

--- src/components/layout/bar/BarSession.jsx
+++ src/components/layout/bar/BarSession.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { Toolbar, Typography, Button } from "@material-ui/core";
+import { Toolbar, Typography, Button, IconButton } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
@@ -8,6 +8,15 @@ const styles = theme => ({
     display: "none",
     [theme.breakpoints.up("md")]: {
       display: "flex"
+    }
+  },
+  grow: {
+    flexGrow: 1
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none"
     }
   }
 });
@@ -17,12 +26,23 @@ class BarSession extends Component {
     const { classes } = this.props;
 
     return (
-      <Toolbar>
-        <Typography variant="h6">ACXEL HOMES</Typography>
-        <div className={classes.sectionDesktop}>
-          <Button>Login</Button>
-        </div>
-      </Toolbar>
+      <div>
+        <Toolbar>
+          <IconButton color="inherit">
+            <i className="material-icons">menu</i>
+          </IconButton>
+          <Typography variant="h6">ACXEL HOMES</Typography>
+          <div className={classes.grow}></div>
+          <div className={classes.sectionDesktop}>
+            <Button color="inherit">Login</Button>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton color="inherit">
+              <i className="material-icons">more_vert</i>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </div>
     );
   }
 }
